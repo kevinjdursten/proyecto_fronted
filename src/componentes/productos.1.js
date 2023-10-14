cd8afd9958c850313c0eb99a29205603cd233644
kevinjdursten <125619391+kevinjdursten@users.noreply.github.com>
2023-10-14 18:22:57 -0400

    paginas
    cambios

---
+++ src/componentes/productos.1.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export function productos() {
+  return;
+  <div className='contenedor-producto'>
+    <img
+      className='imagen-producto'
+      src={require('../kramelo shop arreglos')}
+      alt />
+
+    <div classname='info-producto'>
+      <p classname='titulo'></p>
+      <p classname='descripcion'></p>
+      <p classname='precio'></p>
+
+    </div>
+
+  </div>;
+}
---
+++ src/vistas/About us.jsx
@@ -0,0 +1,39 @@
+import banner from "../kramelo shop arreglos/Edicion Photoshop.jpg"
+const Home = () => {
+    return (
+            <main className="h-screen w-full">
+                
+                {/* Barra de navegacion*/}
+                <nav className="flex justify-end gap-[20px] mr-10 py-[30px]">
+                    <a href="/">Home</a>
+                    
+                    <a href="/Cumpleaños">Cumpleaños</a>
+
+                    <a href="/15 años ">15 años</a>
+
+                    <a href="/Aniversarios">Aniversarios</a>
+
+                    <a href="/Bouquets">Bouquets</a>
+                    
+                    <a href="/Peluches">Peluches</a>
+
+                    <a href="/Globos">Globos</a>
+
+                    <a href="/Graduaciones">Graduaciones</a>
+
+                    <a href="/about">About us</a>
+
+                    <a href="/login">login</a>
+
+
+                </nav>
+
+                <section className="flex w-full justify-center items-center bg-gray-400 h-[500px]">
+                    <img className="w-72 h-72" src={banner} alt="Logo" />
+                </section>
+
+            </main>
+    );
+}
+
+export default Home ;
