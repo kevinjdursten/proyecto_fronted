dbeaded2652f7c2d98c7dbddc6be253094d92226
kevinjdursten <125619391+kevinjdursten@users.noreply.github.com>
2023-10-26 21:06:49 -0400

    nuevos cambios
    cambios

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -11,6 +11,142 @@ const Home = () => {
 
 
     );
+};
+return (
+ <div class="container mx-auto px-4 sm:px-8 max-w-screen-lg">
+     <>
+         <div className="item">
+             <div className="contenedor-items">
+                 <div className="item">
+                     <span className="titulo-item">arreglo de dinosaurio</span>
+                     <img src={Img} alt="/kramelo shop arreglos/dinosaurio.jpg" className="img-item" />
+                     <span className="precio-item">$25</span>
+                     <button className="boton-item">Agregar al Carrito</button>
+                 </div>
+             </div>
+         </div>
+         {/* ... */}
+         <div className="item">
+             <div className="contenedor-items">
+                 <div className="item">
+                     <span className="titulo-item">cilindro con perfume y pack de cartas tematico Blancanieves</span>
+                     <img src={Img} alt="/kramelo shop arreglos/blancanieves.jpg" className="img-item" />
+                     <span className="precio-item">$30</span>
+                     <button className="boton-item">Agregar al Carrito</button>
+                 </div>
+             </div>
+         </div>
+         {/* ... */}
+     </>
+ );
+
+return (
+    <>
+        <div className="item">
+            <div className="contenedor-items">
+                <div className="item">
+                    <span className="titulo-item">caja de madera Buchanan Deluxe 12 Años</span>
+                    <img src={Img} alt="/kramelo shop arreglos/buchanan.jpg" className="img-item" />
+                    <span className="precio-item">$25</span>
+                    <button className="boton-item">Agregar al Carrito</button>
+                </div>
+            </div>
+        </div>
+        {/* ... */}
+        <div className="item">
+            <div className="contenedor-items">
+                <div className="item">
+                    <span className="titulo-item">cilindro con perfume y pack de cartas tematico La Bella Durmiente</span>
+                    <img src={Img} alt="/kramelo shop arreglos/durmiente.jpg" className="img-item" />
+                    <span className="precio-item">$30</span>
+                    <button className="boton-item">Agregar al Carrito</button>
+                </div>
+            </div>
+        </div>
+        {/* ... */}
+    </>
+);
+
+
+return (
+    <>
+        <div className="item">
+            <div className="contenedor-items">
+                <div className="item">
+                    <span className="titulo-item">arreglo y bolso Stitch</span>
+                    <img src={Img} alt="/kramelo shop arreglos/arreglo stitch.jpg" className="img-item" />
+                    <span className="precio-item">$25</span>
+                    <button className="boton-item">Agregar al Carrito</button>
+                </div>
+            </div>
+        </div>
+        {/* ... */}
+        <div className="item">
+            <div className="contenedor-items">
+                <div className="item">
+                    <span className="titulo-item">caja con rosa artificial y globos</span>
+                    <img src={Img} alt="/kramelo shop arreglos/rosa.jpg" className="img-item" />
+                    <span className="precio-item">$30</span>
+                    <button className="boton-item">Agregar al Carrito</button>
+                </div>
+            </div>
+        </div>
+        {/* ... */}
+    </>
+);
+
+<div class="carrito-items">
+                <!-- 
+                <div class="carrito-item">
+                    <img src="" width="80px" alt="">
+                    <div class="carrito-item-detalles">
+                        <span class="carrito-item-titulo"></span>
+                        <div class="selector-cantidad">
+                            <i class="fa-solid fa-minus restar-cantidad"></i>
+                            <input type="text" value="1" class="carrito-item-cantidad" disabled>
+                            <i class="fa-solid fa-plus sumar-cantidad"></i>
+                        </div>
+                        <span class="carrito-item-precio">$0</span>
+                    </div>
+                   <span class="btn-eliminar">
+                        <i class="fa-solid fa-trash"></i>
+                   </span>
+                </div>
+                <div class="carrito-item">
+                    <img src="" width="80px" alt="">
+                    <div class="carrito-item-detalles">
+                        <span class="carrito-item-titulo"></span>
+                        <div class="selector-cantidad">
+                            <i class="fa-solid fa-minus restar-cantidad"></i>
+                            <input type="text" value="3" class="carrito-item-cantidad" disabled>
+                            <i class="fa-solid fa-plus sumar-cantidad"></i>
+                        </div>
+                        <span class="carrito-item-precio">$0</span>
+                    </div>
+                   <button class="btn-eliminar">
+                        <i class="fa-solid fa-trash"></i>
+                   </button>
+                </div>
+                 -->
+            </div>
+            <div class="carrito-total">
+                <div class="fila">
+                    <strong>Tu Total</strong>
+                    <span class="carrito-precio-total">
+                        $0
+                    </span>
+                </div>
+                <button class="btn-pagar">Pagar<i class="fa-solid fa-bag-shopping"></i> </button>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
+
+
+);
+
+    );
 }
 
 export default Home;
